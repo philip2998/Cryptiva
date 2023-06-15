38ed79012ed46a6767e77ad84ba8d30c30028deb
philip2998 <filipgevorgyan@gmail.com>
2023-06-15 20:09:06 +0400

    make changes, that send email functionality will work

--- server/server.js
+++ server/server.js
@@ -14,40 +14,42 @@ app.use(cors());
 
 const OAuth2Client = new OAuth2(
   process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
 );
-OAuth2Client.setCredentials({ refresh_token: process.env.TOKEN });
+OAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
-const sendEmail = (email, subject, message) => {
-  const accessToken = OAuth2Client.getAccessToken();
+const sendEmail = async (email, subject, message) => {
+  try {
+    const accessToken = await OAuth2Client.getAccessToken();
+    const transport = nodemailer.createTransport({
+      service: 'gmail',
+      secure: true,
+      auth: {
+        type: 'OAuth2',
+        user: process.env.EMAIL,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+    });
 
-  const transport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.EMAIL,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.TOKEN,
-      accessToken: accessToken,
-    },
-  });
+    const mailOptions = {
+      from: email,
+      replyTo: email,
+      to: process.env.EMAIL,
+      subject,
+      text: message,
+    };
 
-  const mailOptions = {
-    from: email,
-    to: process.env.EMAIL,
-    subject,
-    text: message,
-  };
+    const result = await transport.sendMail(mailOptions);
+    console.log('Email sent successfully: ', result);
 
-  transport.sendMail(mailOptions, (err, result) => {
-    if (err) {
-      console.log('Error: ', err);
-    } else {
-      console.log('Success: ', result);
-    }
     transport.close();
-  });
+  } catch (err) {
+    console.log('Error sending email: ', err);
+  }
 };
 
 app.get('/api', (req, res) => res.send('Hello from Server'));
@@ -56,7 +58,7 @@ app.post('/api', (req, res) => {
   const token = req.headers.authorization.split(' ')[1];
   const { email, subject, message } = req.body;
 
-  if (token === process.env.TOKEN) {
+  if (token === process.env.EXPRESS_TOKEN) {
     sendEmail(email, subject, message);
     res.json({ message: 'Email sent successfully' });
   } else {
@@ -65,7 +67,4 @@ app.post('/api', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server start at port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server start at port ${PORT}`));
